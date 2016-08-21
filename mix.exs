5e4c6012729b9d090343db3f48b7f549ffc58bd6
Martin Schurig <martin@schurig.pw>
2016-08-21 18:32:40 +0200

    Bump version to 0.1.0-alpha.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExAcme.Mixfile do
   def project do
     [
       app: :ex_acme,
-      version: "0.1.0",
+      version: "0.1.0-alpha.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
