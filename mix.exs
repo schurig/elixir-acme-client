e09e1c7bf54362c68de4d5ba590dab550420fbec
Martin Schurig <martin@schurig.pw>
2016-08-21 18:16:09 +0200

    change supported elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExAcme.Mixfile do
     [
       app: :ex_acme,
       version: "0.1.0",
-      elixir: "~> 1.3",
+      elixir: "~> 1.1",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       preferred_cli_env: [
