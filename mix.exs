09c6387dfae7321b420595b5aa5d8b750bb69387
Martin Schurig <martin@schurig.pw>
2016-08-21 18:14:47 +0200

    add meta data

--- mix.exs
+++ mix.exs
@@ -8,34 +8,41 @@ defmodule ExAcme.Mixfile do
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps(),
       preferred_cli_env: [
         vcr: :test, "vcr.delete": :test, "vcr.check": :test, "vcr.show": :test
-      ]
+      ],
+      name: "ex-acme",
+      description: "A Elixir client for the letsencrypt's ACME protocol.",
+      deps: deps(),
+      package: package(),
+      aliases: aliases()
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE*", "CHANGELOG.md"],
+      maintainers: ["Martin Schurig"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/schurig/ex-acme"},
+    ]
+  end
+
   def application do
     [applications: [:logger, :jose, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:jose, "~> 1.8"},
       {:httpoison, "~> 0.9.0"},
-      {:exvcr, "~> 0.7", only: :test}
+      {:exvcr, "~> 0.7", only: :test},
+      {:credo, "~> 0.4.5", only: [:dev, :test]},
+      {:inch_ex, "~> 0.5.3", only: [:dev, :test, :docs]}
     ]
+  end
+
+  defp aliases do
+    []
   end
 end
